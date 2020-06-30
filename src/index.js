e493056c68cf95e9c0d2669dedc6d023e1e7884e
zhouhaitao <capslock.zhou@tenxcloud.com>
2020-06-30 15:15:21 +0800

    remove useless comments

--- src/index.js
+++ src/index.js
@@ -14,10 +14,3 @@ ReactDOM.createRoot(
   document.getElementById('root')
 ).render(<App />);
 console.log('React 源码调试，当前版本：' + React.version);
-
-
-// I 'm debugging the react source code, everything is fine until I adopting concurrent mode.
-
-// When I use ReactDOM.createRoot like this,
-//
-// the App component can't mount into DOM, but ReactDOM.render is fine.
